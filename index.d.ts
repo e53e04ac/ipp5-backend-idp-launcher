1b06c6d3f5d444b343d9484b98115286b553a948
@e53e04ac <e53e04ac@gmail.com>
2023-01-29 02:10:34 +0900

    0

--- index.d.ts
+++ index.d.ts
@@ -9,27 +9,11 @@ import { EventEmitter } from 'event-emitter';
 import { FileEntry } from 'file-entry';
 import { Get } from 'hold';
 import { ValueOrGet } from 'hold';
+import { Ipp5BackendIdpEnvMap } from 'ipp5-types';
 
 export declare namespace Ipp5BackendIdpLauncher {
 
-    type EnvMap = {
-        readonly TZ: string;
-        readonly WEBSITES_PORT: number;
-        readonly CUSTOM_BACKEND_IDP_HTTP_PATH_PREFIX: string;
-        readonly CUSTOM_BACKEND_IDP_REGISTER_URN: string;
-        readonly CUSTOM_BACKEND_IDP_REGISTER_PRIVATE_KEY: string;
-        readonly CUSTOM_BACKEND_IDP_REGISTER_PUBLIC_KEY: string;
-        readonly CUSTOM_BACKEND_IDP_CHALLENGE_TOKEN_URN: string;
-        readonly CUSTOM_BACKEND_IDP_CHALLENGE_TOKEN_PRIVATE_KEY: string;
-        readonly CUSTOM_BACKEND_IDP_CHALLENGE_TOKEN_PUBLIC_KEY: string;
-        readonly CUSTOM_BACKEND_IDP_TOKEN_URN: string;
-        readonly CUSTOM_BACKEND_IDP_TOKEN_PRIVATE_KEY: string;
-        readonly CUSTOM_BACKEND_IDP_STORAGE_ACCOUNT_NAME: string;
-        readonly CUSTOM_BACKEND_API_URN: string;
-        readonly CUSTOM_EXPRESS_ENV: 'production' | 'development';
-        readonly CUSTOM_EXPRESS_SUBDOMAIN_OFFSET: number;
-        readonly CUSTOM_EXPRESS_TRUST_PROXY: string;
-    };
+    type EnvMap = Ipp5BackendIdpEnvMap;
 
     type EventSpecs = Record<never, never>;
 
