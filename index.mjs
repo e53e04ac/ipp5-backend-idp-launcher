222d5e9c2cffcd2801733ac9eac9dc74117f8243
@e53e04ac <e53e04ac@gmail.com>
2023-02-25 17:09:09 +0900

    0

--- index.mjs
+++ index.mjs
@@ -103,7 +103,7 @@ const constructor = ((options) => {
             return _self.scriptFile().parent();
         }),
         runDirectory: hold(() => {
-            return _options.tmpDirectory().resolve('ipp5-backend-idp-launcher', _options.launchId());
+            return _options.tmpDirectory().resolve(_options.launchId());
         }),
         settingsJson: hold(() => {
             return _self.runDirectory().resolve('settings.json');
